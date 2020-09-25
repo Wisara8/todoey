03350e645432536c78e15e840dab120919680232
Wisara8 <33183269+Wisara8@users.noreply.github.com>
2020-09-24 23:45:20 -0700

    Consumer widget refractor

---
+++ tasks_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'task_tile.dart';
+import 'package:provider/provider.dart';
+import '../models/task_data.dart';
+
+class TasksList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              taskTitle: taskData.tasks[index].name,
+              isChecked: taskData.tasks[index].isDone,
+              checkboxCallback: (checkboxState) {
+//            setState(() {
+//              Provider.of<TaskData>(context).tasks[index].toggleDone();
+//            });
+              },
+            );
+          },
+          itemCount: taskData.tasks.length,
+        );
+      },
+    );
+  }
+}
